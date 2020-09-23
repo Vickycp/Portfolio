721c9c18b9bc390fe319b61b4071f2d11731cf79
vickycp <vickycp417@gmail.com>
2020-09-23 17:14:55 +0530

    Updates

---
+++ precache-manifest.7825ac4da42ba9db2b20a66fd6d12d32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940fb724130d247f3c2ed7b888bdd429",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b417ad34cd29a709760f",
+    "url": "/Portfolio/static/css/main.11869d3b.chunk.css"
+  },
+  {
+    "revision": "7b5c770ea96400280cef",
+    "url": "/Portfolio/static/js/2.b835bace.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.b835bace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b417ad34cd29a709760f",
+    "url": "/Portfolio/static/js/main.2281a003.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "366d99b1f88077c72bd6799328a00e74",
+    "url": "/Portfolio/static/media/vikascp.366d99b1.pdf"
+  }
+]);
